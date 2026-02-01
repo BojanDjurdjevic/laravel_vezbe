00dff5a546ed5adc9f68572603a3c08233bcc9fe
BojanDjurdjevic <pininfarina164@gmail.com>
2026-02-01 23:07:42 +0100

    shop blade added

---
+++ resources/views/shop.blade.php
@@ -0,0 +1,8 @@
+<h1>Shop stranica</h1>
+
+<div>
+    <h3>Proizvodi:</h3>
+    <p>{{ $proizvod1 }}</p>
+    <p>{{ $proizvod2 }}</p>
+    <p>{{ $proizvod3 }}</p>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    echo "Test 123";
+    return view('welcome');
 });
 
-Route::view("/contact", 'contact');
+Route::view("/contact", 'contact'); // čisto da prikažem i ovaj način
 
 Route::get("/about", function() {
     return view('about', [
         'ime' => 'Bojan',
         'prezime' => 'Đurđević'
+    ]);
+});
+
+Route::get('/shop', function()  {
+    return view('shop', [
+        'proizvod1' => 'Knjiga',
+        'proizvod2' => 'Laptop',
+        'proizvod3' => 'Automobil'
     ]);
 });
