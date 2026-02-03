52b1450d9f007c4420171f9978d31b2465e9e706
BojanDjurdjevic <pininfarina164@gmail.com>
2026-02-03 12:45:30 +0100

    Contact Model & migration added

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductModel extends Model
 {
-    //
+    protected $table = "products";
+
+    protected $fillable = [
+        'name', 'description', 'amount', 
+        'price', 'image'
+    ];
 }
