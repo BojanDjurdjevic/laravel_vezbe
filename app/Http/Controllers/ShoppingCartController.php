eb6032d3c5578519adce46ad28ebd87831e95f67
BojanDjurdjevic <pininfarina164@gmail.com>
2026-02-14 23:48:44 +0100

    Domaći 23 -> Product Cart details displayed & check ordered amount vs db amount

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -3,21 +3,43 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\ProductRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
 class ShoppingCartController extends Controller
 {
+    private $prodRepo;
+
+    public function __construct()
+    {
+        $this->prodRepo = new ProductRepository();
+    }
     public function index()
     {
+        $session = Session::get('product');
+
+        $cartProducts = [];
+
+        foreach($session as $p) {
+            $prod = $this->prodRepo->getProductById($p['product_id']);
+            $prod->ordered = $p['amount'];
+            $cartProducts[] = $prod;
+        }
+
         return view('cart', [
-            'cart' => Session::get('product')
+            'cart' => $cartProducts
         ]);
     }
     public function addToCart(Request $request) 
-    {   
-        Session::put('product', [
-            $request->id => $request->amount
+    {  
+        $product = $this->prodRepo->getProductById($request->id);
+        if($product->amount < $request->amount) {
+            return redirect()->back()->with('error', "Nema dovoljno $product->name na stanju. Možete ubaciti do: $product->amount jedinica.");
+        }
+        Session::push('product', [
+            'product_id' => $request->id,
+             'amount' => $request->amount
         ]);
 
         return redirect()->route('cart');
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -2,8 +2,11 @@
     <div class="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50 p-4">
         <div class="w-full max-w-md bg-white rounded-2xl shadow-xl p-8">
             <h3 class="text-center">Korpa:</h3>
-            @foreach ($cart as $product => $amount)
-                <p>{{ $product. " " . $amount}}</p>
+            @foreach ($cart as $product)
+                <div class="w-full p-3 mt-2 rounded-lg shadow-sm">
+                    <p>Naziv proizvoda: {{ $product->name}}</p>
+                    <p>Količina: {{ $product->ordered}}</p>
+                </div>
             @endforeach
             
         </div>
