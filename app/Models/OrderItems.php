64c96fb498a098a9b11e7c0588939d4c1775a159
BojanDjurdjevic <pininfarina164@gmail.com>
2026-02-15 13:47:37 +0100

    Order & OrderItems migrations and models created

---
+++ app/Models/OrderItems.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItems extends Model
+{
+    protected $table = 'order_items';
+
+    protected $fillable = [
+        'order_id', 'product_id', 'order_amount', 'price'
+    ];
+}
